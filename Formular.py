4c3220023d0919c66591f80d18728a33ea40de51
Felipe <felipe.Alvarez-Rodrigues@dr-pfleger.de>
2023-11-03 09:23:22 +0100

    Test 1

---
+++ Formular.py
@@ -0,0 +1,34 @@
+
+nome = "Felipe"
+idade = "34"
+nascimento = 1989
+cidadania = "Brasileira" " " "e " "Espanhola"
+saiu_do_Brasil = 2016
+chegou_na_Alemanha = 2017
+se_casou = 2021
+irmaos = 3 
+primos = 6
+mae = 1
+Pai = 1
+dados = [nome, idade, nascimento, cidadania]
+familia = ["Benito","Nilsa", "Felipe"]
+familia_alter = [70, 50, 34]
+print("Nome:" + nome )
+print("Idade " + "= " + idade + " anos")
+print("Quanto tempo morou no Brasil?")
+print(saiu_do_Brasil - nascimento) # Como colocar o anos ?
+
+print("Quanto tempo está morando na Alemanha?")
+print(2023 - chegou_na_Alemanha) # Como colocar o anos ?
+
+print("Quanto tempo de casado ?")
+print(2023 - se_casou ) # Como colocar o anos ?
+
+
+print ("Aperte str * alt * R")
+print("aparece os últimos comandos dados no terminal")
+
+print(type(dados))
+print(dados)
+
+
