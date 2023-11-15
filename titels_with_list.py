b95cae2938d57bd919e4f2e2b8653499fa805c26
Felipe <felipe.Alvarez-Rodrigues@dr-pfleger.de>
2023-11-15 15:30:12 +0100

    aktualisiert 2

--- titels_with_list.py
+++ titels_with_list.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-länder = ["Brasilien", "Deutschland", "Italien", "Argentinien", "Frankreich", "Uruguai", "Spanien"]
-meisterschaft = [5, 4, 4, 3, 2, 2, 1]
+countries = ["Brasilien", "Deutschland", "Italien", "Argentinien", "Frankreich", "Uruguai", "Spanien"]
+titles = [5, 4, 4, 3, 2, 2, 1]
 
-länder = länder + ["England"] 
-meisterschaft = meisterschaft + [1] 
+countries = countries+ ["England"] 
+titles = titles + [1] 
 
 
 xlab = "Land"
 ylab = "Titel"
-title = "Fußball-Weltmeisterschaft"
+title = "World Cup"
 
 #plt.plot(länder, meisterschaft)   # nicht gut dafür  ////   #plt.scatter(länder, meisterschaft)   # auch nicht optimal
 
@@ -18,10 +18,10 @@ plt.xlabel(xlab)
 plt.ylabel(ylab)
 plt.grid(False)
 
-for i, v in enumerate(meisterschaft):
+for i, v in enumerate(titles):
     plt.text(i, v + 0.1, str(v), ha ="center", va = "bottom")
 
 
 
-plt.bar(länder, meisterschaft, color = "blue" )
+plt.bar(countries, titles, color = "blue" )
 plt.show()
